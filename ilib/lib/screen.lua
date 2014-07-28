ac9d8bba3521e620c775b59a4c0ff7eaf2387691
istasi <istasi@gmail.com>
2014-07-28 19:26:24 +0200

    Update screen.lua

--- ilib/lib/screen.lua
+++ ilib/lib/screen.lua
@@ -2,37 +2,46 @@ local component = require('component')
 
 local screen = {
 	['address'] = component.list('screen', true) (),
-	['gpu'] = component.list('gpu') (),
+	['gpu'] = setmetatable ({
+		['address'] = component.list('gpu') (),
+		['bound'] = '',
+		['bind'] = function ( self, address )
+			if self.bound == address then return end
+			
+			component.invoke ( self.address, 'bind', address )
+			self.bound = address
+		end
+	}, {['__tostring'] = function ( self ) return self.address end}),
 
 	['bgColor'] = 0x000000,
 	['fgColor'] = 0xFFFFFF,
 
 	['active'] = function ( self )
-		component.invoke ( self.gpu, 'bind', self.address )
+		self.gpu:bind ( self.address )
 	end,
 
 	['clear'] = function ( self )
 		local size = ({self:maxResolution()})
 
 		self:setBGColor () self:setFGColor ()
-		component.invoke ( self.gpu, 'fill', 1,1, size[1],size[2], ' ' )
+		component.invoke ( tostring(self.gpu), 'fill', 1,1, size[1],size[2], ' ' )
 		
 		return self
 	end,
 	['set'] = function ( self, x,y, message, vertical )
 		self:setBGColor () self:setFGColor ()
-		component.invoke ( self.gpu, 'set', x,y, message )
+		component.invoke ( tostring(self.gpu), 'set', x,y, message )
 		return self
 	end,
 	['fill'] = function ( self, x,y, width,height, char )
 		self:setBGColor () self:setFGColor ()
-		component.invoke ( self.gpu, 'fill', x,y, width,height, char )
+		component.invoke ( tostring(self.gpu), 'fill', x,y, width,height, char )
 		return self
 	end,
 	['setBGColor'] = function ( self, color )
 		if color == nil then
-			if component.invoke ( self.gpu, 'getBackground' ) ~= self.bgColor then
-				component.invoke ( self.gpu, 'setBackground', self.bgColor ) 
+			if component.invoke ( tostring(self.gpu), 'getBackground' ) ~= self.bgColor then
+				component.invoke ( tostring(self.gpu), 'setBackground', self.bgColor ) 
 			end
 			return true
 		end
@@ -47,8 +56,8 @@ local screen = {
 	end,
 	['setFGColor'] = function ( self, color )
 		if color == nil then
-			if component.invoke ( self.gpu, 'getForeground' ) ~= self.fgColor then
-				component.invoke ( self.gpu, 'setForeground', self.fgColor ) 
+			if component.invoke ( tostring(self.gpu), 'getForeground' ) ~= self.fgColor then
+				component.invoke ( tostring(self.gpu), 'setForeground', self.fgColor ) 
 			end
 			return true
 		end
@@ -61,8 +70,15 @@ local screen = {
 
 		self.fgColor = color
 	end,
+
+	['setResolution'] = function ( self, width,height )
+		return component.invoke ( tostring(self.gpu), 'setResolution', width,height )
+	end,
+	['getResolution'] = function ( self )
+		return component.invoke ( tostring(self.gpu), 'getResolution' )
+	end,
 	['maxResolution'] = function ( self )
-		return component.invoke ( self.gpu, 'maxResolution' )
+		return component.invoke ( tostring(self.gpu), 'maxResolution' )
 	end,
 }
 
